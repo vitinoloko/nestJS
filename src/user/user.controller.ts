c2b70155c3b6882671e8b346c25a605f49d7ff6f
vitinoloko <vitinoloko1234@gmail.com>
2025-03-27 14:23:40 -0400

    curso nestjs

---
+++ src/user/user.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+
+    @Get()
+    async getAllUsers(){
+        return JSON.stringify({test: 'abc'});
+    }
+}
